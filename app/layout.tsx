c07082300a0df5edc4bb1c9756947b8a1b364bb4
Zuzana111 <trnkovaz@gmail.com>
2026-04-15 15:44:53 +0200

    add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0Z8QWXQD9P" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-0Z8QWXQD9P');
+            `
+          }}
+        />
+      </head>
       <body className={`${manrope.variable} font-sans antialiased`}>
         <LocaleProvider>
           <Header />
